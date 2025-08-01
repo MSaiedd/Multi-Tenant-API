bd533afe9ff38800ed8d71e7ced050c99f565c73
Mohamed Saied <mohamedsaied87613@gmail.com>
2025-08-01 21:16:03 +0300

    Seeding

---
+++ Multi-Tenant-API/Migrations/20250801181416_seeding.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Multi_Tenant_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tenant",
+                columns: new[] { "Id", "ApiKey", "Name" },
+                values: new object[,]
+                {
+                    { 1, "APIKEY_TENANT_A", "Tenant A" },
+                    { 2, "APIKEY_TENANT_B", "Tenant B" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "EntityA",
+                columns: new[] { "Id", "Name", "TenantId" },
+                values: new object[,]
+                {
+                    { 1, "Entity A1", 1 },
+                    { 2, "Entity A2", 1 },
+                    { 3, "Entity A3", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EntityA",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "EntityA",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "EntityA",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tenant",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tenant",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
